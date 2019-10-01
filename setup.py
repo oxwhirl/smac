85d496d62fccea285bfc301ebe7757969014a378
samvelyan <mika.samvelyan@gmail.com>
2019-10-01 11:07:22 +0400

    Switching to PySC2 3.0.0

--- setup.py
+++ setup.py
@@ -38,8 +38,8 @@ setup(
         'smac.examples.rllib'
     ],
     install_requires=[
-        'pysc2>=2.0.2',
-        's2clientprotocol>=4.6.0.67926.0',
+        'pysc2>=3.0.0',
+        's2clientprotocol>=4.10.1.75800.0',
         'absl-py>=0.1.0',
         'numpy>=1.10',
     ],
--- smac/env/starcraft2/starcraft2.py
+++ smac/env/starcraft2/starcraft2.py
@@ -279,13 +279,12 @@ class StarCraft2Env(MultiAgentEnv):
 
     def _launch(self):
         """Launch the StarCraft II game."""
-        self._run_config = run_configs.get()
+        self._run_config = run_configs.get(version=self.game_version)
         _map = maps.get(self.map_name)
 
         # Setting up the interface
         interface_options = sc_pb.InterfaceOptions(raw=True, score=False)
-        self._sc2_proc = self._run_config.start(version=self.game_version,
-                                                window_size=self.window_size)
+        self._sc2_proc = self._run_config.start(window_size=self.window_size)
         self._controller = self._sc2_proc.controller
 
         # Request to create the game
