af8ba8a6cc493f7b288bdb231f4cb82ee7cd1459
mmcaulif <mmcaulif@tcd.ie>
2022-11-28 17:52:26 +0000

    migrated from gym to gymnasium

--- smac/env/pettingzoo/StarCraft2PZEnv.py
+++ smac/env/pettingzoo/StarCraft2PZEnv.py
@@ -1,7 +1,7 @@
 from smac.env import StarCraft2Env
-from gym.utils import EzPickle
-from gym.utils import seeding
-from gym import spaces
+from gymnasium.utils import EzPickle
+from gymnasium.utils import seeding
+from gymnasium import spaces
 from pettingzoo.utils.env import ParallelEnv
 from pettingzoo.utils.conversions import (
     parallel_to_aec as from_parallel_wrapper,
