8c4fff48ace10e0f73753b6b90a0b97fd97818cb
Thomas B. Brunner <thomasbbrunner@gmail.com>
2023-08-23 10:58:36 +0200

    Update protobuf version and fix compatibility with latest numpy versions

--- smac/env/starcraft2/starcraft2.py
+++ smac/env/starcraft2/starcraft2.py
@@ -374,7 +374,7 @@ class StarCraft2Env(MultiAgentEnv):
                 np.flip(
                     np.transpose(
                         np.array(
-                            list(map_info.pathing_grid.data), dtype=np.bool
+                            list(map_info.pathing_grid.data), dtype=bool
                         ).reshape(self.map_x, self.map_y)
                     ),
                     axis=1,
@@ -1346,7 +1346,7 @@ class StarCraft2Env(MultiAgentEnv):
         """
         arr = np.zeros(
             (self.n_agents, self.n_agents + self.n_enemies),
-            dtype=np.bool,
+            dtype=bool,
         )
 
         for agent_id in range(self.n_agents):
